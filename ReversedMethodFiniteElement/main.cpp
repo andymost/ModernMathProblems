b6805f41611e1543b5ffc02658c2c3dbc6fa7d36
andymost <andreymostcase@gmail.com>
2017-10-22 22:40:16 +0700

    Initial Commit

---
+++ ReversedMethodFiniteElement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ReversedMethodFiniteElement
+//
+//  Created by Кочнев Андрей Владимирович on 22/10/2017.
+//  Copyright © 2017 Кочнев Андрей Владимирович. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
